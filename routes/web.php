979a98b5b53a98f396a1d288996bc6a8c99b13e6
Nour Edin <nouredin.alhabal@gmail.com>
2024-05-12 17:13:45 +0300

    Added book model and endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/books', [BookController::class, 'index']);
 
 require __DIR__.'/auth.php';
